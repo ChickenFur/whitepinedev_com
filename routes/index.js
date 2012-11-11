da842f56a7424abbd6aae1801d2c26219872a0ee
Christen Thompson <chickenfur@gmail.com>
2012-11-10 23:04:13 -0500

    1st iteration

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,25 @@
 exports.index = function(req, res){
   res.render('index', { title: 'WhitePineDev.com' });
 };
+
+/*
+* Get the blog page
+*/
+
+exports.blog = function(req, res){
+  res.render('blog', { title: 'WhitePineDev.com: blog' } );
+};
+/*
+* Get the projects page
+*/
+
+exports.projects = function(req, res){
+  res.render('projects', { title: 'WhitePineDev.com: projects' } );
+};
+/*
+* Get the about page
+*/
+
+exports.about = function(req, res){
+  res.render('about', { title: 'WhitePineDev.com: about' } );
+};
